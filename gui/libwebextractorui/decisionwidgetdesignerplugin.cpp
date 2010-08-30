c9036621ebba03208d563913fdf92e03db1a38ce
Artem Serebriyskiy <v.for.vandal@gmail.com>
2010-08-30 22:04:25 +0400

    Change license from GPL to LGPL

--- gui/libwebextractorui/decisionwidgetdesignerplugin.cpp
+++ gui/libwebextractorui/decisionwidgetdesignerplugin.cpp
@@ -5,7 +5,7 @@ Copyright (C) 2010 by Serebriyskiy Artem <v.for.vandal at gmail.com>
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) version 3, or any
+version 3.1 of the License, or (at your option) version 3, or any
 later version accepted by the membership of KDE e.V. (or its
 successor approved by the membership of KDE e.V.), which shall
 act as a proxy defined in Section 6 of version 3 of the license.
