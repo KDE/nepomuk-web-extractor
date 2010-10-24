008d199babc8fdba103408136826dc003508f67c
Artem Serebriyskiy <v.for.vandal@gmail.com>
2010-10-24 17:48:48 +0400

    Add Messages.sh

---
+++ Messages.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp 
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/nepomuk-webextractor.pot
+
